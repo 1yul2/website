0872d2d2be28374a7b5136baba6549501197f75b
ruby-kim <dev.rubykim@gmail.com>
2025-09-20 04:16:01 +0900

    feat: update config setup

--- app/config.py
+++ app/config.py
@@ -7,12 +7,11 @@ class Config:
     TURSO_DATABASE_URL = os.getenv("TURSO_DATABASE_URL")
     TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
 
-    try:
+    if TURSO_DATABASE_URL and TURSO_AUTH_TOKEN:
         # .env에 값이 있을 시에만 Turso를 사용하도록 설정
         SQLALCHEMY_DATABASE_URI = f"sqlite+{TURSO_DATABASE_URL}?secure=true"
         CONNECT_ARGS = {"auth_token": TURSO_AUTH_TOKEN}
-    except Exception as e:
+    else:
         # Turso 정보가 없거나 에러가 발생한다면 로컬 db를 사용하도록 설정
-        print(f"Error loading configuration: {e}\nLoad local database...")
         SQLALCHEMY_DATABASE_URI = "sqlite:///instance/reviews.db"
         CONNECT_ARGS = {"check_same_thread": False}

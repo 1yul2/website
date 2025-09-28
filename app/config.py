fd181488ced2d12d8ed86d6e76b953ce6e5e2d4b
1yul2 <hanyul0417@gmail.com>
2025-09-28 19:32:26 +0900

    feat: config.py fix

--- app/config.py
+++ app/config.py
@@ -6,17 +6,15 @@ load_dotenv()
 class Config:
     TURSO_DATABASE_URL = os.getenv("TURSO_DATABASE_URL")
     TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
-    if not TURSO_DATABASE_URL and not TURSO_AUTH_TOKEN:
+
+    if TURSO_DATABASE_URL and TURSO_AUTH_TOKEN:
+        # Turso 연결 (SQLAlchemy에서 libsql은 직접 드라이버 필요)
         SQLALCHEMY_DATABASE_URI = f"sqlite+{TURSO_DATABASE_URL}?secure=true"
         CONNECT_ARGS = {"auth_token": TURSO_AUTH_TOKEN}
     else:
-        # Turso 정보가 없거나 에러가 발생한다면 로컬 db를 사용하도록 설정
         print("\n[INFO] No Turso Setup: Use local db...\n")
 
-        # instance 폴더 경로
         INSTANCE_DIR = os.path.join(os.path.dirname(__file__), "..", "instance")
-
-        # 폴더가 없으면 생성
         os.makedirs(INSTANCE_DIR, exist_ok=True)
 
         SQLALCHEMY_DATABASE_URI = "sqlite:///instance/reviews.db"
